e246e74eae440e5b161cf512cc42cd1f66d9b064
zinminnhtun257 <84656258+zinminnhtun257@users.noreply.github.com>
2021-07-28 22:51:16 +0630

    Update head.php

--- front_panel/head.php
+++ front_panel/head.php
@@ -9,8 +9,31 @@ require_once  "core/functions.php"
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+
+    <!-- Primary Meta Tags -->
+    <meta name="title" content="သီချင်းနားထောင်ကြမယ်">
+    <meta name="description" content="သီချင်းနားထောင်ကြမယ် ဂစ်တာရှိရင် သီချင်းစာသား Notes တွေနဲ့ တီးခတ်လို့ရပါတယ် စိတ်ပြေလက်ပျောက်ပေါ့">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="Music Room">
+    <meta property="og:url" content="http://zinminnhtun.epizy.com/">
+    <meta property="og:title" content="သီချင်းနားထောင်ကြမယ်">
+    <meta property="og:description" content="သီချင်းနားထောင်ကြမယ် ဂစ်တာရှိရင် သီချင်းစာသား Notes တွေနဲ့ တီးခတ်လို့ရပါတယ် စိတ်ပြေလက်ပျောက်ပေါ့">
+    <meta property="og:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuVE3RMAUcNLoqjs-KZZyQyDAC1qbMJm2FIg&usqp=CAU">
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="Music Room">
+    <meta property="twitter:url" content="http://zinminnhtun.epizy.com/">
+    <meta property="twitter:title" content="သီချင်းနားထောင်ကြမယ်">
+    <meta property="twitter:description" content="သီချင်းနားထောင်ကြမယ် ဂစ်တာရှိရင် သီချင်းစာသား Notes တွေနဲ့ တီးခတ်လို့ရပါတယ် စိတ်ပြေလက်ပျောက်ပေါ့">
+    <meta property="twitter:image" content="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuVE3RMAUcNLoqjs-KZZyQyDAC1qbMJm2FIg&usqp=CAU">
+
+
+
     <link rel="shortcut icon" href="<?php echo $url;?>/assets/img/icon.png">
     <link rel="stylesheet" href="<?php echo $url;?>/assets/vendor/bootstrap/bootstrap-5.0.0-beta3-dist/css/bootstrap.min.css">
-<!--    <link rel="stylesheet" href="--><?php //echo $url;?><!--/customise_bootstrap\mybootstrap\mybootstrap.css">-->
+    <!--    <link rel="stylesheet" href="--><?php //echo $url;?><!--/customise_bootstrap\mybootstrap\mybootstrap.css">-->
     <link rel="stylesheet" href="<?php echo $url;?>/assets/vendor/feather_icon/feather.css">
     <link rel="stylesheet" href="<?php echo $url;?>/assets/css/style.css">
